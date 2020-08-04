e994a3a2c3fc90949c1ae03d5faaa33778891229
Philippe Cayouette <pcayouette@spoluck.ca>
2020-08-04 09:23:40 -0400

    update path

--- run_script.bat
+++ run_script.bat
@@ -1,3 +1,3 @@
-"C:\Users\Philippe Cayouette\AppData\Local\Microsoft\WindowsApps\python.exe" z:\extdrive\cams\encode_and_sub.py ParkingCam\to_encode
-"C:\Users\Philippe Cayouette\AppData\Local\Microsoft\WindowsApps\python.exe" z:\extdrive\cams\encode_and_sub.py PorchCam\to_encode
-"C:\Users\Philippe Cayouette\AppData\Local\Microsoft\WindowsApps\python.exe" z:\extdrive\cams\encode_and_sub.py ShedCam\to_encode
+"C:\Users\Philippe Cayouette\AppData\Local\Microsoft\WindowsApps\python.exe" z:\extdrive\PiCams\encode_and_sub.py ParkingCam\to_encode
+"C:\Users\Philippe Cayouette\AppData\Local\Microsoft\WindowsApps\python.exe" z:\extdrive\PiCams\encode_and_sub.py PorchCam\to_encode
+"C:\Users\Philippe Cayouette\AppData\Local\Microsoft\WindowsApps\python.exe" z:\extdrive\PiCams\encode_and_sub.py ShedCam\to_encode
